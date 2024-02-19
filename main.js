9b7971443f31181700545d37a8171bdf6a2b3a39
giang <nguyenchiemgiang@gmail.com>
2024-02-19 09:47:45 +0700

    update feesback

--- main.js
+++ main.js
@@ -16,18 +16,18 @@ document.querySelectorAll(".project-title").forEach(function (title) {
 
 const circles=document.querySelectorAll('.circle');
 circles.forEach(elem=>{
-  var dots=elem.getAttribute("data-dots");
-  var marked=elem.getAttribute("data-percent");
-  var percent=Math.floor(dots*marked/100);
-  var points="";
-  var rotate=360 / dots;
+  var dots= elem.getAttribute("data-dots");
+  var marked= elem.getAttribute("data-percent");
+  var percent= Math.floor(dots*marked/100);
+  var points= "";
+  var rotate= 360 / dots;
 
   for (let i=0 ;i < dots ;i++){
-    points+=` <div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    points += ` <div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
   }
   elem.innerHTML=points;
 
-const pointsMarkeds=elem.querySelectorAll('.points');
+const pointsMarkeds = elem.querySelectorAll('.points');
   for (let i=0 ;i<percent;i++){
     pointsMarkeds[i].classList.add('marked')
   }
